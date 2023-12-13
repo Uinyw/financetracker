dd45a21533affe5d17d9ca932eddcdd8502b3aea
I556063 <tim.lachenicht@sap.com>
2023-12-13 08:49:30 +0100

    provide sceleton

---
+++ implementation/savingsgoal/src/main/java/com/financetracker/savingsgoal/SavingsgoalApplication.java
@@ -0,0 +1,13 @@
+package com.financetracker.savingsgoal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SavingsgoalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SavingsgoalApplication.class, args);
+	}
+
+}
---
+++ implementation/savingsgoal/src/test/java/com/financetracker/savingsgoal/SavingsgoalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.financetracker.savingsgoal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SavingsgoalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
