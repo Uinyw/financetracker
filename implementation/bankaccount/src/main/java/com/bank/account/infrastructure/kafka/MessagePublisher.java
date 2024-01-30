4f260177bb695d91bd962435bf8d0644e2e42b1c
I556063 <tim.lachenicht@sap.com>
2024-01-30 07:53:45 +0100

    make tests run

--- implementation/bankaccount/src/main/java/com/bank/account/infrastructure/kafka/MessagePublisher.java
+++ implementation/bankaccount/src/main/java/com/bank/account/infrastructure/kafka/MessagePublisher.java
@@ -1,24 +1,8 @@
 package com.bank.account.infrastructure.kafka;
 
-import com.bank.account.api.mapping.BankAccountMapper;
 import com.bank.account.logic.model.BankAccount;
-import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.kafka.core.KafkaTemplate;
-import org.springframework.stereotype.Component;
 
-@RequiredArgsConstructor
-@Component
-public class MessagePublisher {
+public interface MessagePublisher {
 
-    private final KafkaTemplate<String, Object> template;
-    private final BankAccountMapper bankAccountMapper;
-
-    @Value("${tpd.topic-bank-account-update}")
-    private String bankAccountUpdate;
-
-    public void publishMessageBankAccountUpdate(final BankAccount bankAccount) {
-        this.template.send(bankAccountUpdate, bankAccountMapper.mapBankAccountModelToDto(bankAccount));
-    }
-
+    void publishMessageBankAccountUpdate(final BankAccount bankAccount);
 }
--- implementation/bankaccount/src/test/java/com/bank/account/api/BankResourceTest.java
+++ implementation/bankaccount/src/test/java/com/bank/account/api/BankResourceTest.java
@@ -3,16 +3,24 @@ package com.bank.account.api;
 import com.bank.account.IntegrationTestBase;
 import com.bank.account.data.TestBankAccountBuilder;
 import io.restassured.http.ContentType;
-import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.openapitools.model.MonetaryAmountDto;
 import org.springframework.http.HttpStatus;
 
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.is;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.doNothing;
 
 class BankResourceTest extends IntegrationTestBase {
 
-    @Disabled
+    @BeforeEach
+    void setUp() {
+        doNothing().when(messagePublisher).publishMessageBankAccountUpdate(any());
+    }
+
     @Test
     void givenBankAccountDto_whenCreateBankAccount_thenBankAccountExists() {
         final var bankAccountDto = TestBankAccountBuilder.buildWithDefaults();
@@ -38,7 +46,6 @@ class BankResourceTest extends IntegrationTestBase {
                 .and().body("[0].id", is(bankAccountDto.getId().toString()));
     }
 
-    @Disabled
     @Test
     void givenBankAccount_whenGetBankAccountById_thenBankAccountReturned() {
         final var bankAccountDto = TestBankAccountBuilder.buildWithDefaults();
@@ -55,5 +62,57 @@ class BankResourceTest extends IntegrationTestBase {
                 .then()
                 .statusCode(HttpStatus.OK.value())
                 .and().body("id", is(bankAccountDto.getId().toString()));
+    }
+
+    @Test
+    void givenBankAccount_whenDeleteBankAccountById_thenBankAccountDoesNotExistAnymore() {
+        final var bankAccountDto = TestBankAccountBuilder.buildWithDefaults();
+
+        given().port(port)
+                .contentType(ContentType.JSON)
+                .body(bankAccountDto)
+                .post(LOCAL_BASE_URL_WITHOUT_PORT + "/bankAccounts")
+                .then()
+                .statusCode(HttpStatus.OK.value());
+
+        given().port(port)
+                .delete(LOCAL_BASE_URL_WITHOUT_PORT + "/bankAccounts/" + bankAccountDto.getId().toString())
+                .then()
+                .statusCode(HttpStatus.OK.value());
+
+        given().port(port)
+                .get(LOCAL_BASE_URL_WITHOUT_PORT + "/bankAccounts")
+                .then()
+                .statusCode(HttpStatus.OK.value())
+                .and().body("size()", is(0));
+    }
+
+    @Test
+    void givenBankAccount_whenPatchBankAccountById_thenBankAccountIsUpdated() {
+        final var bankAccountDto = TestBankAccountBuilder.buildWithDefaults();
+
+        given().port(port)
+                .contentType(ContentType.JSON)
+                .body(bankAccountDto)
+                .post(LOCAL_BASE_URL_WITHOUT_PORT + "/bankAccounts")
+                .then()
+                .statusCode(HttpStatus.OK.value());
+
+        final Double updatedBalance = bankAccountDto.getBalance().getAmount() + 100.0;
+        bankAccountDto.setBalance(MonetaryAmountDto.builder().amount(updatedBalance).build());
+
+        given().port(port)
+                .contentType(ContentType.JSON)
+                .body(bankAccountDto)
+                .patch(LOCAL_BASE_URL_WITHOUT_PORT + "/bankAccounts/" + bankAccountDto.getId().toString())
+                .then()
+                .statusCode(HttpStatus.OK.value());
+
+        given().port(port)
+                .get(LOCAL_BASE_URL_WITHOUT_PORT + "/bankAccounts/" + bankAccountDto.getId().toString())
+                .then()
+                .statusCode(HttpStatus.OK.value())
+                .and().body("id", is(bankAccountDto.getId().toString()))
+                .and().body("balance.amount", equalTo(updatedBalance.floatValue()));
     }
 }
