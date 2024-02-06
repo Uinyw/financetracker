d8cfc8cee3a323c9bfb6fe9540f85a0c6cb9f11a
I556063 <tim.lachenicht@sap.com>
2024-02-06 08:52:50 +0100

    finish savings goal ui

--- implementation/ui-financetracker/src/app/savings-goal-periodical-detail/savings-goal-periodical-detail.component.ts
+++ implementation/ui-financetracker/src/app/savings-goal-periodical-detail/savings-goal-periodical-detail.component.ts
@@ -1,4 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { PeriodicalSavingsGoal, SavingsRecord } from '../savings-goal-periodical/periodicalSavingsGoal';
+import { MatTableDataSource } from '@angular/material/table';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatDialog } from '@angular/material/dialog';
+import { ActivatedRoute } from '@angular/router';
+import { PeriodicalSavingsGoalService } from '../savings-goal-periodical/periodicalSavingsGoal.service';
 
 @Component({
   selector: 'app-savings-goal-periodical-detail',
@@ -6,5 +12,31 @@ import { Component } from '@angular/core';
   styleUrls: ['./savings-goal-periodical-detail.component.scss']
 })
 export class SavingsGoalPeriodicalDetailComponent {
+
+  displayedColumns: string[] = ['date', 'amount', 'achievementStatus'];
+  savingsGoal: PeriodicalSavingsGoal | undefined;
+  savingsRecords = new MatTableDataSource<SavingsRecord>();
+
+  @ViewChild(MatPaginator, { static: true })
+  paginator!: MatPaginator;
+
+  constructor(savingsGoalService: PeriodicalSavingsGoalService,
+              route: ActivatedRoute) {
+      savingsGoalService.getPeriodicalSavingsGoalById(route.snapshot.paramMap.get("id")!).subscribe(s => {
+      this.savingsGoal = s;
+      this.savingsRecords.data = s.savingsRecords;
+    });
+  }
+
+  ngOnInit() {
+    this.savingsRecords.paginator = this.paginator;
+  }
+
+  applyFilter(event: Event) {
+    if (event.target == null) {
+      return;
+    }
+    this.savingsRecords.filter = (event.target as HTMLInputElement).value.trim().toLowerCase();
+  }
 
 }
