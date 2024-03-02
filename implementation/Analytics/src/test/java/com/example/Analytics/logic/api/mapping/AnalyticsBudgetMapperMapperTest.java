09befb97d02161f8d1a055a5d5afbbc860e2b9e4
ToKa <st155019@stud.uni-stuttgart.de>
2024-03-02 08:01:23 +0100

    added BudgetMapper

---
+++ implementation/Analytics/src/test/java/com/example/Analytics/logic/api/mapping/AnalyticsBudgetMapperMapperTest.java
@@ -0,0 +1,66 @@
+package com.example.Analytics.logic.api.mapping;
+
+import com.example.Analytics.IntegrationTestBase;
+import com.example.Analytics.api.mapping.BudgetMapper;
+import com.example.Analytics.api.mapping.ProductMapper;
+import com.example.Analytics.logic.model.budgetModel.AchievementStatus;
+import com.example.Analytics.logic.model.budgetModel.BudgetElement;
+import com.example.Analytics.logic.model.budgetModel.BudgetPlan;
+import com.example.Analytics.logic.model.budgetModel.Category;
+import com.example.Analytics.logic.model.generalModel.MonetaryAmount;
+import com.example.Analytics.logic.model.productModel.Nutrition;
+import com.example.Analytics.logic.model.productModel.Product;
+import org.junit.jupiter.api.Test;
+import org.openapitools.client.model.*;
+import org.openapitools.model.BudgetAchievementStatusDto;
+import org.openapitools.model.BudgetElementDto;
+import org.openapitools.model.BudgetPlanDto;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.List;
+import java.util.UUID;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+
+public class AnalyticsBudgetMapperMapperTest extends IntegrationTestBase {
+    @Autowired
+    private BudgetMapper budgetMapper;
+
+    @Test
+    void givenBudgetPlan_whenMap_thenBudgetPlanDtoExists() {
+        UUID id = UUID.randomUUID();
+        AchievementStatus currentStatus = AchievementStatus.ACHIEVED;
+        BudgetElement budgetElement = createBudgetElement(new MonetaryAmount(0.0), new Category("test"));
+        List<BudgetElement> budgetElementList = List.of();
+        LocalDate startDate = LocalDate.now();
+
+        BudgetPlan budgetPlan = createBudgetPlan(id, currentStatus, budgetElementList, startDate);
+        BudgetPlanDto budgetPlanDto = budgetMapper.budgetPlanToDto(budgetPlan);
+        BudgetElementDto budgetElementDto = budgetPlanDto.getPlan().get(0);
+
+        assertThat(budgetPlan.getId(), is(budgetPlanDto.getId()));
+        assertThat(budgetPlan.getStartDate().toString(), is(budgetPlanDto.getDate()));
+        assertThat(budgetPlan.getCurrentStatus(), is(budgetPlanDto.getAchievementStatus()));
+        assertThat(budgetElement.getMonetaryAmount().getAmount(), is(budgetElementDto.getMonetaryAmount().getAmount()));
+        assertThat(budgetElement.getCategory().getName(), is(budgetElementDto.getCategory()));
+    }
+
+
+private BudgetElement createBudgetElement(MonetaryAmount monetaryAmount, Category category){
+        return BudgetElement.builder()
+                .monetaryAmount(monetaryAmount)
+                .category(category)
+                .build();
+}
+    private BudgetPlan createBudgetPlan(UUID id, AchievementStatus currentStatus, List<BudgetElement> budgetElementList, LocalDate startDate){
+        return BudgetPlan.builder()
+                .id(id)
+                .currentStatus(currentStatus)
+                .budgetElementList(budgetElementList)
+                .startDate(startDate)
+                .build();
+    }
+}
