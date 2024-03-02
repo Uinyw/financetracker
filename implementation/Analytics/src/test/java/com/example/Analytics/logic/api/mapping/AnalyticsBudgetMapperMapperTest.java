6fd01b5ab4419d103b66930569ce39bef615fa1d
ToKa <st155019@stud.uni-stuttgart.de>
2024-03-02 08:09:02 +0100

    update AnalyticsBudgetMapperMapperTest

--- implementation/Analytics/src/test/java/com/example/Analytics/logic/api/mapping/AnalyticsBudgetMapperMapperTest.java
+++ implementation/Analytics/src/test/java/com/example/Analytics/logic/api/mapping/AnalyticsBudgetMapperMapperTest.java
@@ -7,6 +7,7 @@ import com.example.Analytics.logic.model.budgetModel.AchievementStatus;
 import com.example.Analytics.logic.model.budgetModel.BudgetElement;
 import com.example.Analytics.logic.model.budgetModel.BudgetPlan;
 import com.example.Analytics.logic.model.budgetModel.Category;
+import com.example.Analytics.logic.model.generalModel.FilterElement;
 import com.example.Analytics.logic.model.generalModel.MonetaryAmount;
 import com.example.Analytics.logic.model.productModel.Nutrition;
 import com.example.Analytics.logic.model.productModel.Product;
@@ -15,6 +16,7 @@ import org.openapitools.client.model.*;
 import org.openapitools.model.BudgetAchievementStatusDto;
 import org.openapitools.model.BudgetElementDto;
 import org.openapitools.model.BudgetPlanDto;
+import org.openapitools.model.ReportFilterDto;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import java.math.BigDecimal;
@@ -34,7 +36,7 @@ public class AnalyticsBudgetMapperMapperTest extends IntegrationTestBase {
         UUID id = UUID.randomUUID();
         AchievementStatus currentStatus = AchievementStatus.ACHIEVED;
         BudgetElement budgetElement = createBudgetElement(new MonetaryAmount(0.0), new Category("test"));
-        List<BudgetElement> budgetElementList = List.of();
+        List<BudgetElement> budgetElementList = List.of(budgetElement);
         LocalDate startDate = LocalDate.now();
 
         BudgetPlan budgetPlan = createBudgetPlan(id, currentStatus, budgetElementList, startDate);
@@ -43,11 +45,18 @@ public class AnalyticsBudgetMapperMapperTest extends IntegrationTestBase {
 
         assertThat(budgetPlan.getId(), is(budgetPlanDto.getId()));
         assertThat(budgetPlan.getStartDate().toString(), is(budgetPlanDto.getDate()));
-        assertThat(budgetPlan.getCurrentStatus(), is(budgetPlanDto.getAchievementStatus()));
+        assertThat(budgetPlan.getCurrentStatus().toString(), is(budgetPlanDto.getAchievementStatus().getValue()));
         assertThat(budgetElement.getMonetaryAmount().getAmount(), is(budgetElementDto.getMonetaryAmount().getAmount()));
         assertThat(budgetElement.getCategory().getName(), is(budgetElementDto.getCategory()));
     }
 
+    @Test
+    void givenReportFilterDto_whenMap_thenFilterElementExists() {
+        ReportFilterDto reportFilterDto = ReportFilterDto.builder().build();
+        FilterElement filterElement = budgetMapper.filterDtoToElement(reportFilterDto);
+
+        assertThat(null, is(filterElement));
+    }
 
 private BudgetElement createBudgetElement(MonetaryAmount monetaryAmount, Category category){
         return BudgetElement.builder()
