f5903bdf04ff2cc7cd8cb01897dbe6a2b6102c17
ToKa <st155019@stud.uni-stuttgart.de>
2024-03-04 11:01:21 +0100

    add forecast Entities and test + edit the old ones

--- implementation/Analytics/src/main/java/com/example/Analytics/logic/model/forecast/ForecastEntities.java
+++ implementation/Analytics/src/main/java/com/example/Analytics/logic/model/forecast/ForecastEntities.java
@@ -1,12 +1,16 @@
 package com.example.Analytics.logic.model.forecast;
 
+import com.example.Analytics.logic.model.generalModel.MonetaryAmount;
+import lombok.Builder;
 import lombok.Getter;
 
 import java.time.LocalDate;
 import java.util.HashMap;
+import java.util.UUID;
 
+@Builder
 @Getter
 public class ForecastEntities {
     private LocalDate date;
-    private HashMap<String, ForecastEntry> forecastEntriesList;
+    private HashMap<UUID, MonetaryAmount> forecastEntriesList;
 }
