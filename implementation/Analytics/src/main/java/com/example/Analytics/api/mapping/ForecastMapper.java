21e375d096cd3955eebaff34ae28da9e1c50769d
ToKa <st155019@stud.uni-stuttgart.de>
2024-03-04 11:00:51 +0100

    add Forecast Mapper and test

--- implementation/Analytics/src/main/java/com/example/Analytics/api/mapping/ForecastMapper.java
+++ implementation/Analytics/src/main/java/com/example/Analytics/api/mapping/ForecastMapper.java
@@ -4,6 +4,7 @@ import com.example.Analytics.logic.model.forecast.Forecast;
 import com.example.Analytics.logic.model.forecast.ForecastEntities;
 import com.example.Analytics.logic.model.forecast.ForecastEntry;
 import org.openapitools.model.ForecastDto;
+import org.openapitools.model.ForecastElementDto;
 import org.openapitools.model.MonetaryAmountDto;
 import org.springframework.stereotype.Component;
 
@@ -13,50 +14,13 @@ import java.util.List;
 
 @Component
 public class ForecastMapper {
-    /*
-    frorcast DTO
-    months = anzahl monate
-    date = jetzt <- pretty much useless
-    Forecast Element = category+monat -> geld/bankAccount
-     */
-
     public ForecastDto forecastToDto(Forecast forecast){
-        /*ForecastDto forecastDto = ForecastDto.builder()
-                .date(LocalDate.now().toString())
-                .months(0)
-                .plan(getHashMapDto(forecast.getForecastEntitiesList()))
+        List<ForecastElementDto> forecastElementDtos = forecast.getForecastEntriesList().entrySet().stream().map(set->
+            new ForecastElementDto(set.getKey(), new MonetaryAmountDto(set.getValue().getAmount()))
+        ).toList();
+        return ForecastDto.builder()
+                .date(forecast.getDate().toString())
+                .plan(forecastElementDtos)
                 .build();
-
-        if(forecast.getForecastEntitiesList() != null){
-            forecastDto.setMonths(forecast.getForecastEntitiesList().size());
-        }
-        return forecastDto;*/
-        return null;
     }
-
-    /*private HashMap<String, ForecastElement> getHashMapDto(List<ForecastEntities> forecastEntitiesList){
-        HashMap<String, ForecastElement> newHasmapDto = new HashMap<>();
-        if(forecastEntitiesList==null)
-            return newHasmapDto;
-
-        for(ForecastEntities forecastEntity : forecastEntitiesList){
-            String date = forecastEntity.getDate().toString();
-            forecastEntity.getForecastEntriesList().forEach(
-                    (category, forecastEntry) -> {
-                        String newKey = date + " [" + category + "]";
-                        newHasmapDto.put(newKey, forecastEntriesToDto(forecastEntry));
-                    }
-            );
-        }
-
-        return newHasmapDto;
-    }
-
-    private ForecastElement forecastEntriesToDto(ForecastEntry forecastEntry){
-        ForecastElement newForecastElement = ForecastElement.builder()
-                .bankAccount(forecastEntry.getBankAccountId())
-                .monetaryAmount(new MonetaryAmountDto(forecastEntry.getMonetaryAmount().getAmount()))
-                .build();
-        return newForecastElement;
-    }*/
 }
